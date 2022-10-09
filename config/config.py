8f221beb1979734c9161757d20c1b68ffdfee2ef
onelove-music <115425136+onelove-music@users.noreply.github.com>
2022-10-10 03:16:57 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001621712416"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "sanam")
 
 # Your User ID.
 OWNER_ID = list(
