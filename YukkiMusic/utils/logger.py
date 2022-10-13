ddf2c909850d9dce214561c1510aad940d62ddd2
onelove-music <115425136+onelove-music@users.noreply.github.com>
2022-10-13 23:21:22 +0530

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**YUKKI PLAY LOG**
+**S҉A҉N҉A҉M҉ ҉X҉ ҉M҉U҉S҉I҉C҉ ҉L҉O҉G҉**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
